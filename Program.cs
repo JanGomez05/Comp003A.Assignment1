09e6a3423fa62e425922ddbbe3750685b705136e
Jan Gomez <123139695+JanGomez05@users.noreply.github.com>
2023-01-20 18:56:37 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// this is a single line comment //
+/* this is a multi-line comment
+ * 
+ * Author: Jan Gomez
+ * Course: Comp-003A-L01
+ * Purpose: Coding Asignment 1
+ */
+namespace Comp003A.Assignment1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("100 Things About Me.");
+        Console.WriteLine("1. I am very sad.");
+        Console.WriteLine("2. I have done this 4 times now.");
+        Console.WriteLine("3. I have a twin sister.");
+        Console.WriteLine("4. I am going insane and that is ok.");
+        Console.WriteLine("5. I enjoy eating wingstop.");
+        Console.WriteLine("6. I can not feel my fingers.");
+        Console.WriteLine("7. I enjoy driving my car.");
+        Console.WriteLine("8. I named my car Kirby.");
+        Console.WriteLine("9. I have been in two car accidents.");
+        Console.WriteLine("10. I am a very patient person.");
+        Console.WriteLine("11. I love eating cheetos.");
+        Console.WriteLine("12. Hot cheetos give me heart burn.");
+        Console.WriteLine("13. I do enjoy to cook a lot.");
+        Console.WriteLine("14. I am the second oldest sibling.");
+        Console.WriteLine("15. I love to grill.");
+        Console.WriteLine("16. I have never tried baking.");
+        Console.WriteLine("17. I fart when I am nervous.");
+        Console.WriteLine("18. Coding is tiring my arms.");
+        Console.WriteLine("19. I love to watch sports.");
+        Console.WriteLine("20. I enjoy football the most.");
+        Console.WriteLine("21. I am confused with the assignment.");
+        Console.WriteLine("22. My classmate Anna is trying to help");
+        Console.WriteLine("23. ");
+        Console.WriteLine("Hello, World!");
+    }
+}
+
